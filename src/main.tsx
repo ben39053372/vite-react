b78bc28ae9f0ee546c2b7705f16e735b55c5ca81
Ben Cheng <bencheng@skylight-group.com>
2021-07-15 10:24:48 +0800

    add: i18n

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './styles/common/index.css'
-import { BrowserRouter } from 'react-router-dom'
+import './styles/common/index.css';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import { CssBaseline } from '@material-ui/core';
+import './i18n';
 
 ReactDOM.render(
   <React.StrictMode>
     <CssBaseline />
     <Provider store={store}>
-      <BrowserRouter >
+      <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>

72d0c56cd90da8d37bdfeafc1005589c2138eacf
Ben Cheng <bencheng@skylight-group.com>
2021-07-15 15:54:58 +0800

    style: add error color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,6 @@
+const colors = require('tailwindcss/colors')
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   mode: "jit",
   purge: [
@@ -6,6 +9,10 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      ...defaultTheme.colors,
+      error: colors.red[700]
+    },
     extend: {},
   },
   variants: {

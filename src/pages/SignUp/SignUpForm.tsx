b57d3614ac3b5b3201ea8f13ac497b959356aea6
Ben Cheng <bencheng@skylight-group.com>
2021-07-16 14:33:53 +0800

    feat: Sign Up page UI

---
+++ src/pages/SignUp/SignUpForm.tsx
@@ -0,0 +1,142 @@
+import React from 'react';
+import { useFormik } from 'formik';
+import i18next from 'i18next';
+import { Label } from '../../components/Label';
+import { Input } from '../../components/Input';
+import { useTranslation } from 'react-i18next';
+import { Button } from '../../components/Button';
+import { useHistory } from 'react-router-dom';
+
+interface SignUpFormValues {
+  email: string;
+  name: string;
+  phone: string;
+  password: string;
+}
+
+const validate = (values: SignUpFormValues) => {
+  const errors: Partial<SignUpFormValues> = {};
+
+  // email
+  const emailFormat =
+    /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+  if (!values.email) {
+    errors.email = 'Required';
+  } else if (!emailFormat.test(values.email)) {
+    errors.email = i18next.t('Invalid email address');
+  }
+
+  // password
+  if (!values.password) {
+    errors.password = 'Required';
+  } else if (values.password.length < 8) {
+    errors.password = i18next.t('Must be characters or more', { length: 8 });
+  }
+
+  // Name
+  if (!values.name) {
+    errors.name = 'Required';
+  } else if (values.name.length > 20) {
+    errors.name = 'too large';
+  }
+
+  // phone
+  const phoneFormat = /^[0-9]{8}$|^852[0-9]{8}$|^\+852[0-9]{8}$/;
+  if (!values.phone) {
+    errors.phone = 'Required';
+  } else if (!phoneFormat.test(values.phone)) {
+    errors.phone = i18next.t('Invalid phone');
+  }
+
+  return errors;
+};
+
+export const SignUpForm = () => {
+  const { t } = useTranslation();
+  const history = useHistory();
+  const loginForm = useFormik<SignUpFormValues>({
+    initialValues: {
+      email: '',
+      name: '',
+      phone: '',
+      password: '',
+    },
+    validate,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
+  const handleSignInClick = () => {
+    history.push('/');
+  };
+
+  return (
+    <form onSubmit={loginForm.handleSubmit}>
+      <Label className="asd">{t('email')}</Label>
+      <Input
+        id="email"
+        type="email"
+        placeholder={t('place input', { somethings: t('email') })}
+        {...loginForm.getFieldProps('email')}
+      />
+
+      {/* error message */}
+      {loginForm.touched.email && (
+        <div className="error">{loginForm.errors.email}</div>
+      )}
+
+      <div className="pt-2" />
+
+      <Label>{t('Name')}</Label>
+      <Input
+        id="name"
+        type="text"
+        placeholder={t('place input', { somethings: t('name') })}
+        {...loginForm.getFieldProps('name')}
+      />
+
+      {/* error message */}
+      {loginForm.touched.email && (
+        <div className="error">{loginForm.errors.name}</div>
+      )}
+
+      <div className="pt-2" />
+
+      <Label>{t('phone')}</Label>
+      <Input
+        id="phone"
+        type="text"
+        placeholder={t('place input', { somethings: t('phone') })}
+        {...loginForm.getFieldProps('phone')}
+      />
+
+      {/* error message */}
+      {loginForm.touched.email && (
+        <div className="error">{loginForm.errors.phone}</div>
+      )}
+
+      <div className="pt-2" />
+
+      <Label>{t('password')}</Label>
+      <Input
+        id="password"
+        type="password"
+        placeholder={t('place input', { somethings: t('password') })}
+        {...loginForm.getFieldProps('password')}
+      />
+
+      {/* error message */}
+      {loginForm.touched.password && (
+        <div className="error">{loginForm.errors.password}</div>
+      )}
+
+      <div className="actions">
+        <a className="textButton">{t('I have account')}</a>
+        <Button onClick={handleSignInClick} type="submit">
+          {t('Sign In')}
+        </Button>
+      </div>
+    </form>
+  );
+};
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import { SignUpForm } from './SignUpForm';
 
 export const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <div className="background">
+      <div className="paper">
+        <h1 className="text-4xl text-center pb-4">Sign Up</h1>
+        <SignUpForm />
+      </div>
+    </div>
+  );
 };
